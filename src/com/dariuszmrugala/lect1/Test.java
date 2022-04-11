59bd86e72c1fdc891092cf73a8b2a10c737d8786
Dariusz <dariuszmrugala@gmail.com>
2022-04-11 18:08:53 +0200

    add something in windows environment

--- src/com/dariuszmrugala/lect1/Test.java
+++ src/com/dariuszmrugala/lect1/Test.java
@@ -3,5 +3,7 @@ package com.dariuszmrugala.lect1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("from ubuntu initalized git repo pushed to the git repository");
+
+        System.out.println("added at Win11 and pushed to the repo");
     }
 }
